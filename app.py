e9893a218c2075ffda7122c5eab8099170a3df93
DOpazoM <dario.opazo@gmail.com>
2026-09-11 11:35:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import torch
 from huggingface_hub import hf_hub_download
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-MODEL_NAME = "DOpazo38/smartcv-model"
+MODEL_NAME = ""DOpazo38/modelo-smartcv"
 
 @st.cache_resource
 def cargar_modelo():
